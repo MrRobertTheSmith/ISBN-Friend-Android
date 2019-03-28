496ef13afd2f1044c353a60ce404e455f4afe549
robertsmith <atrobertthesmith@gmail.com>
2019-03-28 22:43:46 +0000

    Unit Tests
    This commit fleshes out a series of unit tests for the Controller Layer. They test that the controller returns valid data for a valid input. They also handle the fact that the Controller is asynchronous using Countdown Latch

--- ISBNFriend/app/src/test/java/com/example/isbnfriend/RetrofitClientTest.java
+++ ISBNFriend/app/src/test/java/com/example/isbnfriend/RetrofitClientTest.java
@@ -1,6 +1,12 @@
 package com.example.isbnfriend;
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule;
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleOwner;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import com.example.isbnfriend.Model.Item;
 import com.example.isbnfriend.View.NetworkViewmodel;
 import org.junit.Before;
 import org.junit.Rule;
@@ -9,12 +15,22 @@ import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static junit.framework.Assert.assertNull;
 import static junit.framework.TestCase.assertEquals;
 
 @RunWith(JUnit4.class)
 public class RetrofitClientTest {
     private NetworkViewmodel testViewModel;
     private String testISBN;
+
+    CountDownLatch aLatch = new CountDownLatch(1);
 
     @Rule
     public TestRule rule = new InstantTaskExecutorRule();
@@ -26,9 +42,18 @@ public class RetrofitClientTest {
     }
 
     @Test
-    public void callGetBookForISBNWithValidISBN() throws Exception {
+    public void callGetBookForISBNWithValidISBNReturnsValidTitle() throws Exception {
 
         testViewModel.searchViaModel(testISBN);
+
+        testViewModel.getJsonResponse().observeForever(new android.arch.lifecycle.Observer<Item>() {
+            @Override
+            public void onChanged(@Nullable Item item) {
+                aLatch.countDown();
+            }
+        });
+
+        aLatch.await();
 
         assertEquals("C Programming Language", testViewModel.getJsonResponse().getValue()
                         .getBooks()
@@ -37,4 +62,44 @@ public class RetrofitClientTest {
                         .getTitle());
     }
 
+    @Test
+    public void callGetBookForISBNWithValidISBNReturnsValidAuthors() throws Exception {
+
+        testViewModel.searchViaModel(testISBN);
+
+        testViewModel.getJsonResponse().observeForever(new android.arch.lifecycle.Observer<Item>() {
+            @Override
+            public void onChanged(@Nullable Item item) {
+                aLatch.countDown();
+            }
+        });
+
+        aLatch.await();
+
+        List<String> authors = new ArrayList<>();
+        authors.add("Brian W. Kernighan");
+        authors.add("Dennis Ritchie");
+
+        assertEquals(authors, testViewModel.getJsonResponse().getValue()
+                .getBooks()
+                .get(0)
+                .getVolumeInfo()
+                .getauthors());
+    }
+
+    @Test
+    public void callGetBookWithInvalidISBN() throws Exception {
+
+        testViewModel.searchViaModel("");
+
+        testViewModel.getJsonResponse().observeForever(new android.arch.lifecycle.Observer<Item>() {
+            @Override
+            public void onChanged(@Nullable Item item) {
+            }
+        });
+
+
+        assertNull(testViewModel.getJsonResponse().getValue());
+
+    }
 }
