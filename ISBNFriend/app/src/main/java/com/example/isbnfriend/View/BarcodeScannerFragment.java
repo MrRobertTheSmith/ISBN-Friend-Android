c0d2312b703a34a6ba5eab77f482f650e73cf207
robertsmith <atrobertthesmith@gmail.com>
2019-04-03 16:56:50 +0100

    Feature: Fragment can now access the camera successfully

--- ISBNFriend/app/src/main/java/com/example/isbnfriend/View/BarcodeScannerFragment.java
+++ ISBNFriend/app/src/main/java/com/example/isbnfriend/View/BarcodeScannerFragment.java
@@ -1,16 +1,21 @@
 package com.example.isbnfriend.View;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.SurfaceView;
-import android.view.View;
-import android.view.ViewGroup;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.*;
 
 import com.example.isbnfriend.R;
 import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class BarcodeScannerFragment extends Fragment {
 
@@ -19,9 +24,8 @@ public class BarcodeScannerFragment extends Fragment {
     SurfaceView cameraView;
     private BarcodeDetector bDetector;
 
-
     //Factory Method
-    public static BarcodeScannerFragment newInstance(){
+    public static BarcodeScannerFragment newInstance() {
         return new BarcodeScannerFragment();
     }
 
@@ -42,11 +46,59 @@ public class BarcodeScannerFragment extends Fragment {
                 .setBarcodeFormats(Barcode.ALL_FORMATS)
                 .build();
 
+        bDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray barcodes = detections.getDetectedItems();
+
+                if (barcodes.size() > 0){
+                }
+            }
+        });
+
         camSource = new CameraSource.Builder(getActivity(), bDetector)
-                .setRequestedPreviewSize(1080, 720)
+                .setRequestedPreviewSize(1024, 1680)
+                .setAutoFocusEnabled(true)
                 .build();
 
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        Log.e("CAMERASOURCE", "No Camera Permission");
+                        return;
+                    }
+                    Log.e("CAMERASOURCE", "Camera Should Start");
+                    camSource.start(cameraView.getHolder());
+                }
+                catch(IOException ie){
+                    Log.e("CAMERASOURCE", ie.getMessage());
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                camSource.stop();
+            }
+        });
         return fragmentView;
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        camSource.release();
+        bDetector.release();
+    }
 }
